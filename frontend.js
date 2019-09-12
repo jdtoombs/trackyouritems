3c6ee53880b180b09d2e9a7b00baec7cbf33fb42
jdtoombs <jdtoombs@gmail.com>
2019-09-11 19:12:01 -0700

    removed steam api key

--- frontend.js
+++ frontend.js
@@ -4,38 +4,9 @@ $(document).ready(function(){
         $.each(data.descriptions, function(key, value){
             inv_data += '<tr>';
             inv_data += '<td>' + value.market_hash_name + '<td>';
-            // inv_data += '<td>' + value.documents[key].market_hash_name + '<td>';
             inv_data += '</tr>';
         });
         $('#item_table').append(inv_data);
     });
 
 });
-
-// var request = new XMLHttpRequest();
-// request.open('GET', 'http://localhost:4000/trackyouritems', true);
-
-// request.onload = function () {
-//     // Begin accessing JSON data here
-//     if (request.status>= 200 && request.status < 400) {
-
-    
-//         var checkParse = JSON.parse(this.response);
-//         console.log(checkParse.total_inventory_count);
-
-//         var i;
-//         var wstream = fs.createWriteStream('myOutput.txt');
-//         var output = document.getElementById('output');
-//         output.innerHTML = checkParse.total_inventory_count;
-//         for(i in checkParse.descriptions){
-    
-//             console.log(checkParse.descriptions[i].market_hash_name);
-//             // wstream.write(checkParse.descriptions[i].market_hash_name + '\n');
-//         }
-//     }else {
-//         console.error("ERROR");
-//     }  
-// }
-  
-//   // Send request
-//   request.send();
--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-// Node API / Express Server
-
-//CSGO APP ID = 730
-// var path = require("path");
 const express = require('express');
-// Will be using request to hit the Steam API 
 const request = require('request');
 var app = express(); 
-var fs = require('fs');
-var toobsSteam64ID = '76561198042252133';
-// Steam API URL - not going to be using at this time .. yet
-var url = 'http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/' +
-'v2/?key=4379E2CAA72E28EF0DACC44EBC59A081&appid=730';
 
+//want this to be interchangeable 
+var toobsSteam64ID = '76561198042252133';
 
 // Need port 
 var port = process.env.PORT || 4000;
@@ -19,15 +11,6 @@ var server = app.listen(port);
 
 console.log("TrackYourItems listening on port: " + port);
 console.log("localhost:" + port + "/trackyouritems");
-
-// Set static folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// Params example for reference 
-// app.get('/test/:name', function(req, res){
-    // var name = req.params.name;
-    // res.send("Oh hey, " + name);
-// });
 
 // May not be necessary 
 app.all('/trackyouritems', function(req, res, next) {
@@ -42,6 +25,8 @@ app.all('/prices', function(req, res, next) {
     next()
 });
 
+// cannot remember whether there was a reason I did this or not 
+// instead of using the steam api directly - CORS?
 // can probably do this without server but better for if we use API later on
 app.get('/trackyouritems', function(req, resp){
     
