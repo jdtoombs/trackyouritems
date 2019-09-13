beb50f5870a7b1bedf758681d6490be50d76cd0a
jdtoombs <jdtoombs@gmail.com>
2019-09-13 16:57:08 -0700

    error handling for 2factor auth failure

--- frontend.js
+++ frontend.js
@@ -1,9 +1,3 @@
-// install 'notp', 'thirty-two' packages first
-// need to figure out how to do below in client side js
-// var totp = require('notp').totp;
-// var base32 = require('thirty-two');
-// var codeName = totp.gen(base32.decode('SECRET_SHOWN_BY_BITSKINS')).toString;
-
 $(document).ready(function(){
     $.getJSON("http://localhost:4000/trackyouritems", function(data){
         var inv_data = '';
@@ -21,22 +15,28 @@ $(document).ready(function(){
         $("#item_table tr").each(function(){
             arr.push($(this).find("td:first").text());
         });
-        // starts at index 1 first one is undefined
-        // no longer spamming get requests to server 
-        // $.getJSON('https://bitskins.com/api/v1/get_all_item_prices/?api_key=*********&code=' + codeName + '&app_id=730', function(data){
-        //     for(i=1; i<arr.length; i++){
-        //         $.each(data, function(i, item){
-        //             if(item.market_hash_name == a[i]){
-        //                 // alert(item.lowest_price);
-        //                 inv_price += '<tr>';
-        //                 inv_price += '<td>' + item.lowest_price + '</td>';
-        //                 inv_price += '</tr>';
-        //             }
-        //         });
-        //         $('item_table').append(inv_price);
-        //     }
+       // starts at index 1 first one is undefined
+       // no longer spamming get requests to server 
+        $.getJSON('http://localhost:4000/prices', function(data){
+            if(data.status == "fail"){
+                alert(data.data.error_message);
+            }else{
+                for(i=1; i<arr.length; i++){
+                    $.each(data.prices, function(i, item){
+                        alert(item);
+                        if(item.market_hash_name == a[i]){
+                            // alert(item.lowest_price);
+                            // inv_price += '<tr>';
+                            // inv_price += '<td>' + item.price + '</td>';
+                            // inv_price += '</tr>';
+                        }
+                    });
+                    //$('item_table').append(inv_price);
+                }
+            }
 
-        // });
+
+        });
 
     });
 });
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 // Server must handle api requests as client side cannot due to CORS
+// need to figure out how to do below in client side js
+var totp = require('notp').totp;
+var base32 = require('thirty-two');
+var codeName = totp.gen(base32.decode('SECRET_SHOWN_BY_BITSKINS')).toString;
 
 const express = require('express');
 const request = require('request');
@@ -21,15 +25,12 @@ app.all('/trackyouritems', function(req, res, next) {
     next()
 });
 
-app.all('/prices/:item_name', function(req, res, next) {
+app.all('/prices', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
 });
 
-// cannot remember whether there was a reason I did this or not 
-// instead of using the steam api directly - CORS?
-// can probably do this without server but better for if we use API later on
 app.get('/trackyouritems', function(req, resp){
     
     var link = 'http://steamcommunity.com/inventory/' + toobsSteam64ID + '/730/2?l=english&cou';
@@ -39,16 +40,12 @@ app.get('/trackyouritems', function(req, resp){
     });
 });
 
-// app.get('/prices', function(req, resp){
-//     // do one get request to bitskins api for all prices
-//     // loop through and match name to get price through front end.js
-//     // send back not on market if 404?
-//     console.log(req.params.item_name);
-//     // 1 for usd 3 for euro
-//     var link = 'https://bitskins.com/api/v1/get_all_item_prices/?api_key=a5615875-a0a0-4f11-ac29-4836aecc11ba&code=' + codeName + '&app_id=730';
-//     request.get(link, function(error, res, body){
-//         resp.setHeader('Content-Type', 'application/json');
-//         resp.send(body);
-//     });
-// });
+app.get('/prices', function(req, resp){
+    console.log(req.params.item_name);
+    var link = 'https://bitskins.com/api/v1/get_all_item_prices/?api_key=a5615875-a0a0-4f11-ac29-4836aecc11ba&code=' + codeName + '&app_id=730';
+    request.get(link, function(error, res, body){
+        resp.setHeader('Content-Type', 'application/json');
+        resp.send(body);
+    });
+});
 
