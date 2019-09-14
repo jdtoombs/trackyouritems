5fc56409705b7b05af492fb40cc6a4608ec2de1c
jdtoombs <jdtoombs@gmail.com>
2019-09-13 18:16:39 -0700

    almost have it appending price

--- frontend.js
+++ frontend.js
@@ -15,25 +15,29 @@ $(document).ready(function(){
         $("#item_table tr").each(function(){
             arr.push($(this).find("td:first").text());
         });
+        alert(arr[1]);
        // starts at index 1 first one is undefined
        // no longer spamming get requests to server 
         $.getJSON('http://localhost:4000/prices', function(data){
+            var inv_price ='';
             if(data.status == "fail"){
                 alert(data.data.error_message);
-                alert(arr[1]);
             }else{
-                for(i=1; i<arr.length; i++){
-                    $.each(data.prices, function(i, item){
-                        alert(item);
-                        if(item.market_hash_name == a[i]){
-                            // alert(item.lowest_price);
-                            // inv_price += '<tr>';
-                            // inv_price += '<td>' + item.price + '</td>';
-                            // inv_price += '</tr>';
+                // for(i=1; i<arr.length; i++){
+            $.each(data.prices, function(i, item){
+                    //alert(item.market_hash_name);
+                for(k = 1; k<arr.length; k++){    
+                    if(item.market_hash_name == arr[k]){
+                        alert(item.price);
+                        // alert(item.lowest_price);
+                            inv_price += '<tr>';
+                            inv_price += '<td>' + item.price + '</td>';
+                            inv_price += '</tr>';
                         }
-                    });
-                    //$('item_table').append(inv_price);
-                }
+                    }
+            }); 
+            $('#item_table tr:second').after(inv_price);
+                
             }
 
 
