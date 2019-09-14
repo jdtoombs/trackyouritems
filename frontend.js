1b0ef1e010c4dc58f3d3c21f65ee95f0204678fd
jdtoombs <jdtoombs@gmail.com>
2019-09-13 18:58:27 -0700

    csgo inv now displayed in table w/ price

--- frontend.js
+++ frontend.js
@@ -1,22 +1,12 @@
 $(document).ready(function(){
+    var inv_data = [];
     $.getJSON("http://localhost:4000/trackyouritems", function(data){
-        var inv_data = '';
         $.each(data.descriptions, function(key, value){
             if(value.marketable == 1 && value.type != "Base Grade Graffiti"){
-                inv_data += '<tr>';
-                inv_data += '<td>' + value.market_hash_name + '<td>';
-                inv_data += '</tr>';
+                inv_data.push(value.market_hash_name);
             }
         });
-        $('#item_table').append(inv_data);
-        
-        // Get data for first column and use it to hit localhost:4000/prices
-        var arr = [];
-        $("#item_table tr").each(function(){
-            arr.push($(this).find("td:first").text());
-        });
-        alert(arr[1]);
-       // starts at index 1 first one is undefined
+
        // no longer spamming get requests to server 
         $.getJSON('http://localhost:4000/prices', function(data){
             var inv_price ='';
@@ -25,18 +15,16 @@ $(document).ready(function(){
             }else{
                 // for(i=1; i<arr.length; i++){
             $.each(data.prices, function(i, item){
-                    //alert(item.market_hash_name);
-                for(k = 1; k<arr.length; k++){    
-                    if(item.market_hash_name == arr[k]){
-                        alert(item.price);
-                        // alert(item.lowest_price);
-                            inv_price += '<tr>';
-                            inv_price += '<td>' + item.price + '</td>';
-                            inv_price += '</tr>';
-                        }
+                for(k = 0; k<inv_data.length; k++){    
+                    if(item.market_hash_name == inv_data[k]){
+                        inv_price += '<tr>';
+                        inv_price += '<td>' + item.market_hash_name + '</td>';
+                        inv_price += '<td>' + item.price + '</td>';
+                        inv_price += '</tr>';
                     }
+                }
             }); 
-            $('#item_table tr:second').after(inv_price);
+            $('#item_table').append(inv_price);
                 
             }
 
