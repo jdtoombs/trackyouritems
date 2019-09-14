8c6004ced37cfa6844ee5e5b9e27c7263582e850
jdtoombs <jdtoombs@gmail.com>
2019-09-14 14:28:02 -0700

    added total inv price - need to optimize

--- frontend.js
+++ frontend.js
@@ -11,23 +11,32 @@ $(document).ready(function(){
        // no longer spamming get requests to server 
         $.getJSON('http://localhost:4000/prices', function(data){
             var inv_price ='';
+            var price_count = [];
+            var sum = 0;
             if(data.status == "fail"){
                 alert(data.data.error_message);
             }else{
                 // for(i=1; i<arr.length; i++){
-            $.each(data.prices, function(i, item){
-                for(k = 0; k<inv_data.length; k++){    
-                    if(item.market_hash_name == inv_data[k]){
-                        inv_price += '<tr>';
-                        inv_price += '<td><img src=' + item.icon_url + ' height=100 width=100></img></td>';
-                        inv_price += '<td>' + item.market_hash_name + '</td>';
-                        inv_price += '<td>$' + item.price + '</td>';
-                        inv_price += '</tr>';
+                $.each(data.prices, function(i, item){
+                    for(k = 0; k<inv_data.length; k++){    
+                        if(item.market_hash_name == inv_data[k]){
+                            inv_price += '<tr>';
+                            inv_price += '<td><img src=' + item.icon_url + ' height=100 width=100></img></td>';
+                            inv_price += '<td>' + item.market_hash_name + '</td>';
+                            inv_price += '<td>$' + item.price + '</td>';
+                            price_count.push(item.price);
+                            inv_price += '</tr>';
+                        }
                     }
+                }); 
+                $('#item_table').append(inv_price);
+                // still a string must convert to int
+                for(j = 0; j < price_count.length; j++){
+                    // alert(price_count[j]);
+                    sum += parseFloat(price_count[j]);
                 }
-            }); 
-            $('#item_table').append(inv_price);
-                
+                $('#tyi').append('$'+sum.toFixed(2)+' USD');
+                // alert('Total value of inventory is $' + sum.toFixed(2));
             }
 
 
