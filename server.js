0cd72ca0ea74995c6c416f7605b9426f3d3162b7
jdtoombs <jdtoombs@gmail.com>
2019-09-15 15:33:13 -0700

    adding functional prototype pics for wiki

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Server must handle api requests as client side cannot due to CORS
 // need to figure out how to do below in client side js
 require('dotenv').config();
-console.log(process.env);
 var totp = require('notp').totp;
 var base32 = require('thirty-two');
 var codeName = totp.gen(base32.decode(process.env.BIT_SKINS_SECRET));
@@ -16,9 +15,6 @@ console.log(codeName);
 const express = require('express');
 const request = require('request');
 var app = express(); 
-
-//want this to be interchangeable 
-var toobsSteam64ID = '76561198042252133';
 
 // Need port 
 var port = process.env.PORT || 4000;
