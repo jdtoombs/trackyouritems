449034c80aaee774170754924c881208da675365
jdtoombs <jdtoombs@gmail.com>
2019-09-16 17:20:44 -0700

    starting to transfer frontend to react - learning

---
+++ frontend_tyi/tyi/src/Components/InvTable.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function InvTable(props){
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+        
+            <tbody>
+
+            </tbody>
+        </table>
+    )
+}
